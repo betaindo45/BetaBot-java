fcc83279ef9a97c72272481267301250d7812fb2
betaindo45 <64950057+betaindo45@users.noreply.github.com>
2022-01-16 06:59:19 +0700

    Update Token.java

--- src/JavaBot/Token.java
+++ src/JavaBot/Token.java
@@ -1,7 +1,5 @@
 package JavaBot;
 
 public class Token {
-    public static String DiscordToken(){
-        return "ODg3NzEwNzg4NjU4MjcwMjU5.YUIHGA.fakTM5KAcY7R5ixyeifxCiwZvnI";
-    }
+    public static String DiscordToken = System.getenv("TOKEN");
 }
